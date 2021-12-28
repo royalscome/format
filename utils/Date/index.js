5efeaac5dc1c34d05ef2df3a1e99297b4adcdf9b
royalscome <1210532312@qq.com>
2021-12-28 10:13:49 +0800

    fix: 修复日期格式化format not function问题

--- utils/Date/index.js
+++ utils/Date/index.js
@@ -3,8 +3,45 @@
  * @Autor: weiyang
  * @Date: 2021-08-31 15:50:57
  * @LastEditors: weiyang
- * @LastEditTime: 2021-08-31 16:40:46
+ * @LastEditTime: 2021-12-23 15:53:32
  */
+Date.prototype.format = function(str) {
+  let fmt = str;
+  const o = {
+    "M+": this.getMonth() + 1, // 月份
+    "d+": this.getDate(), // 日
+    "H+": this.getHours() % 12 === 0 ? 12 : this.getHours() % 12, // 小时
+    "h+": this.getHours(), // 小时
+    "m+": this.getMinutes(), // 分
+    "s+": this.getSeconds(), // 秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+    S: this.getMilliseconds() // 毫秒
+  };
+  const week = {
+    "0": "日",
+    "1": "一",
+    "2": "二",
+    "3": "三",
+    "4": "四",
+    "5": "五",
+    "6": "六"
+  };
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, `${this.getFullYear()}`.substr(4 - RegExp.$1.length));
+  }
+  if (/(E+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (RegExp.$1.length > 1 ? (RegExp.$1.length > 2 ? "星期" : "周") : "") + week[this.getDay() + ""]
+    );
+  }
+  for (const k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : `00${o[k]}`.substr(`${o[k]}`.length));
+    }
+  }
+  return fmt;
+};
 class DateFormat {
   static formatDateWithZero(time) {
     return new Date(time).format("yyyy-MM-dd 00:00:00");
